db9fe7287460b12572c3e3fd029155df38d0bcf9
gio <user@users-MacBook-Pro.local>
2023-06-25 12:03:38 +0400

    remove dotenv to use heroku environment variables

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
-import dotenv
-dotenv.load_dotenv()
+# import dotenv
+# dotenv.load_dotenv()
 TBILISI_BOUNDS = [41.6203945, 41.8434205, 44.5967238, 45.0180102]
 TOKEN = os.environ.get('TOKEN')
 URL = f'https://api.waqi.info/v2/map/bounds?latlng=41.6203945,41.8434205,44.5967238,45.0180102&networks=all&token={TOKEN}'
