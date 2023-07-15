35aa2af9ab4e34d30a6e293ef557c171d5604f4a
gio <user@users-MacBook-Pro.local>
2023-07-15 15:57:35 +0400

    bigquery auth

--- settings.py
+++ settings.py
@@ -1,11 +1,7 @@
 import os
-import dotenv
-dotenv.load_dotenv()
 TBILISI_BOUNDS = [41.6203945, 41.8434205, 44.5967238, 45.0180102]
 TOKEN = os.environ.get('TOKEN')
 URL = f'https://api.waqi.info/v2/map/bounds?latlng=41.6203945,41.8434205,44.5967238,45.0180102&networks=all&token={TOKEN}'
 DATASET_ID = os.environ.get('DATASET_ID')
 TABLE_ID = os.environ.get('TABLE_ID')
 PROJECT_ID = os.environ.get('PROJECT_ID')
-# laod file from local
-GOOGLE_APPLICATION_CREDENTIALS = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
